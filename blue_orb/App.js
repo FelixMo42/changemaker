dbd9d5626f5ec6dba92100273490957a0cef2c9d
FelixMo42 <felix@tacocat.com>
2019-05-04 19:41:32 -0700

    camera stuff works

--- blue_orb/App.js
+++ blue_orb/App.js
@@ -1,41 +1,48 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { RNCamera } from 'react-native-camera'
-import { PermissionsAndroid } from 'react-native'
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native'
+import { Camera, Permissions } from 'expo'
+import { Ionicons } from '@expo/vector-icons';
 
-async function requestCameraPermission() {
-  try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.CAMERA,
-      {
-        title: 'Cool Photo App Camera Permission',
-        message:
-          'Cool Photo App needs access to your camera ' +
-          'so you can take awesome pictures.',
-        buttonNeutral: 'Ask Me Later',
-        buttonNegative: 'Cancel',
-        buttonPositive: 'OK',
-      },
-    );
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log('You can use the camera');
-    } else {
-      console.log('Camera permission denied');
-    }
-  } catch (err) {
-    console.warn(err);
+export default class App extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
   }
-}
 
-export default class App extends React.Component implements PermissionAwareActivity {
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA)
+    this.setState({ hasCameraPermission: status === 'granted' })
+  }
+
   render() {
-    requestCameraPermission()
-    
-    return (
-      <View style={styles.container}>
-        
-      </View>
-    );
+    const { hasCameraPermission } = this.state
+
+    if (hasCameraPermission) {
+      return (
+        <View style={styles.container}>
+              <Camera
+                style={{ flex: 1 }}
+                type={ this.state.type }
+              >
+                <TouchableHighlight
+                  onPress={this.onClicked}
+                  style={{flex: 1}}
+                >
+                  <View></View>
+                </TouchableHighlight>
+              </Camera>
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.container}>
+        </View>
+      )
+    }
+  }
+
+  onClicked() {
+
   }
 }
 

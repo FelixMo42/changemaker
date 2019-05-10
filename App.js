c7e0742e8e90912a299e01613f9b20bd6a0db7eb
FelixMo42 <felix@tacocat.com>
2019-05-09 21:28:09 -0700

    base navigation

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { StyleSheet, View, TouchableHighlight } from 'react-native'
+import { StyleSheet, View, TouchableHighlight, Text, Button } from 'react-native'
 import { Camera, Permissions } from 'expo'
 
 export default class App extends React.Component {
   state = {
     hasCameraPermission: null,
+    page: "main",
     type: Camera.Constants.Type.back,
   }
 
@@ -14,43 +15,101 @@ export default class App extends React.Component {
   }
 
   render() {
-    console.log("app rendering")
+    const { page } = this.state
 
-    const { hasCameraPermission } = this.state
+    console.log("rendering " + page + " page")
 
-    if (hasCameraPermission) {
-      return (
-        <View style={styles.container}>
-              <Camera
-                style={{ flex: 1 }}
-                type={ this.state.type }
-              >
-                <TouchableHighlight
-                  onPress={this.onClicked}
-                  style={{flex: 1}}
-                >
-                  <View></View>
-                </TouchableHighlight>
-              </Camera>
-        </View>
-      )
-    } else {
-      return (
-        <View style={styles.container}>
-        </View>
-      )
+    switch (page) {
+      case "main" : return this.renderMainPage()
+      case "info" : return this.renderInfoPage()
+      case "history" : return this.renderHistoryPage()
+      case "settings" : return this.renderSettingsPage()
     }
   }
 
-  onClicked() {
-    console.log("click")
+  /// rendering ///
+
+  renderMainPage() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          style={{ flex: 1 }}
+          type={ this.state.type }
+          ref={ ref => { this.camera = ref } }
+        >
+          <TouchableHighlight
+            onPress={ () => { this.takePicture() } }
+            style={{flex: 1}}
+          >
+            <View>
+              <Button
+                onPress={ () => { this.goto("history") } }
+                title="history"
+              />
+              <Button
+                onPress={ () => { this.goto("settings") } }
+                title="settings"
+              />
+            </View>
+          </TouchableHighlight>
+        </Camera>
+      </View>
+    ) 
+  }
+
+  renderInfoPage() {
+    return (
+      <View style={[styles.container, {padding: 10, paddingTop: 25}]}>
+        <View style={styles.infoBox}>
+          <Text>name: Plastic</Text>
+          <Text>how to dispose</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <Text>name: Plastic</Text>
+          <Text>how to dispose</Text>
+        </View>
+      </View>
+    )
+  }
+
+  renderHistoryPage() {
+    return (
+      <View style={styles.container}>
+      </View>
+    )
+  }
+
+  renderSettingsPage() {
+    return (
+      <View style={styles.container}>
+      </View>
+    )
+  }
+
+  /// functions ///
+
+  goto(page) {
+    this.setState(state => ({page: page}))
+  }
+
+  async takePicture() {
+    this.goto("info")
+
+    //let photo = await this.camera.takePictureAsync()
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
+    flexDirection: 'column'
   },
+  infoBox: {
+    padding: 10,
+    margin: 5,
+    fontSize: 25,
+    borderColor: "black",
+    borderWidth: 5,
+    borderRadius: 10
+  }
 });
